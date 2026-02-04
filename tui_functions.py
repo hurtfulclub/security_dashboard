3b42d9feea69eb3ed49cb4f7cc3a0b76259ecf0c
hurtfulclub <hurtfulclub@gmail.com>
2026-02-04 14:58:41 +0100

    Removed redundant functions + added README.md

--- tui_functions.py
+++ tui_functions.py
@@ -1,35 +1,5 @@
 import subprocess
 
-def ping8888():
-      output = subprocess.Popen(["ping", "-c", "6", "8.8.8.8"],
-                                text=True,
-                                stderr=subprocess.STDOUT,
-                                stdout=subprocess.PIPE
-      )
-
-      for line in output.stdout:
-            yield line.rstrip()
-
-def trace8888():
-      output = subprocess.Popen(["tracepath", "8.8.8.8"],
-                                text=True,
-                                stderr=subprocess.STDOUT,
-                                stdout=subprocess.PIPE
-      )
-
-      for line in output.stdout:
-            yield line.rstrip()
-
-def pinggoogle():
-      output = subprocess.Popen(["ping", "-c", "6", "google.com"],
-                                text=True,
-                                stderr=subprocess.STDOUT,
-                                stdout=subprocess.PIPE
-      )
-
-      for line in output.stdout:
-            yield line.rstrip()
-
 def pingcustom(address):
       output = subprocess.Popen(["ping", "-c", "6", address],
                                 text=True,
